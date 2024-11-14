cd5d240b1dab4ab139c86a1b2512e5e80007671f
XoeZ <dubxten@gmail.com>
2024-11-14 17:53:12 -0300

    Input camera Mouse and Gamepad - Movement Keyboard

--- Assets/Resources/Scripts/Player/PlayerController.cs
+++ Assets/Resources/Scripts/Player/PlayerController.cs
@@ -4,11 +4,20 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    public Vector3 inputMovement;
+
     public float speed;
-    public Vector2 inputMovement;
+    public Transform cameraRotation;
+
+
     void Update()
     {
-        
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, cameraRotation.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        inputMovement = context.ReadValue<Vector2>();
     }
 
     void FixedUpdate()
@@ -23,6 +32,6 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDirection = forward * inputMovement.y + right * inputMovement.x;
 
-        rb.linearVelocity = moveDirection * speed + new Vector3(0, rb.linearVelocity.y, 0); // Conserva la velocidad vertical para la gravedad
+        rb.linearVelocity = moveDirection * speed + new Vector3(0, rb.linearVelocity.y, 0);
     }
 }
