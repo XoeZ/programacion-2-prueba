b1597529845cc17b0d4630079e7555f07a7d15a9
XoeZ <dubxten@gmail.com>
2024-11-15 09:57:17 -0300

    Eliminacion de multiples assets

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -6,9 +6,40 @@ using UnityEngine;
 
 public enum GameState
 {
+    Menu,
     Gameplay,
-    Pause,
-    GameOver,
-    Victory
+    Victory,
+    GameOver
+}
+
+public class GameManager : Singleton<GameManager>
+{
+    public static event System.Action<GameState> OnStateChange;
+
+
+    [SerializeField] private GameState m_initialState;
+
+
+    public static GameState CurrentState { get; private set; } = (GameState)(-1);
+
+
+    protected override bool Persistent => false;
+
+
+    private void Start()
+    {
+        SwitchState(m_initialState);
+    }
+    // if (health == 0) 
+    // {
+    //      GameManager.SwitchState(GameState.GameOver);
+    // }
+    public static void SwitchState(GameState state)
+    {
+        if (state == CurrentState) return;
+
+        CurrentState = state;
+        OnStateChange?.Invoke(state);
+    }
 }
 
